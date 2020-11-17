84759a88f6a06b71cc0f74887bc37ea54bfec4d3
Xen14581 <72184281+Xen14581@users.noreply.github.com>
2020-11-17 23:10:02 +0530

    re uploading files with password encryption

--- sarsConnections/src/main/java/com/example/restdemo/SecurityConfiguration.java
+++ sarsConnections/src/main/java/com/example/restdemo/SecurityConfiguration.java
@@ -8,9 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+// import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -30,8 +30,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-        // return new BCryptPasswordEncoder();
+        // return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
         // return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
